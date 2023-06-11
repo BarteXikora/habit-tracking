9639339f4e2fe0bdb5b60efa92347a459b2ddebb
BarteXikora <sbartek50@gmail.com>
2023-06-11 14:37:27 +0200

    Linked bootstrap styles to App.tsx.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import Page404 from './components/sites/Page404'
 import { Provider } from 'react-redux'
 import store from './functions/reduxStore'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/main.css'
 
 const App = () => {
