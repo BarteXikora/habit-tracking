527ff8ab42c35f1fc4a4a752ecc38b8f3620d8e2
BarteXikora <sbartek50@gmail.com>
2023-06-11 14:38:20 +0200

    Added bootstrap classes to navbar.

--- client/src/components/system/AppNavbar.tsx
+++ client/src/components/system/AppNavbar.tsx
@@ -3,10 +3,10 @@ import { Navbar, Container } from 'react-bootstrap'
 import imgLogo from '../../img/logo.png'
 
 const AppNavbar = () => {
-    return <Navbar>
-        <Container>
-            <Navbar.Brand>
-                <img src={imgLogo} alt='goodhabit' width={35} />
+    return <Navbar className='py-1 px-4'>
+        <Container fluid>
+            <Navbar.Brand className='d-flex'>
+                <img src={imgLogo} alt='goodhabit' width={35} className='me-2' />
 
                 <span>goodhabit</span>
             </Navbar.Brand>
