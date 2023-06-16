739bfc15f243341ea42a43db64c70dd8feeda369
BarteXikora <sbartek50@gmail.com>
2023-06-16 16:42:47 +0200

    Created basic header with hardcoded date.

---
+++ client/src/components/decorations/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return <div className='app-header p-5'>
+        <h1 className='py-4'>Piątek, 16 czerwca</h1>
+    </div>
+}
+
+export default Header
--- client/src/components/sites/Dashboard.tsx
+++ client/src/components/sites/Dashboard.tsx
@@ -1,9 +1,13 @@
 import { Container, Row, Col } from 'react-bootstrap'
 
+import Header from '../decorations/Header'
+
 const Dashboard = () => {
     return <Container fluid>
         <Row>
-            <Col className='p-0'></Col>
+            <Col className='p-0'>
+                <Header />
+            </Col>
         </Row>
     </Container>
 }
