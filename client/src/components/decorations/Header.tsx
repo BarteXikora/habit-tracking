36a5e785d21c8c8acee46acbadc666193359a3de
BarteXikora <sbartek50@gmail.com>
2023-06-16 22:03:39 +0200

    Added date formating in header, date from user device.

--- client/src/components/decorations/Header.tsx
+++ client/src/components/decorations/Header.tsx
@@ -1,6 +1,13 @@
+import moment from 'moment'
+
+import 'moment/locale/pl'
+
 const Header = () => {
+    const date: string = moment().format('dddd, D MMMM')
+    const formatedDate = date.charAt(0).toUpperCase() + date.slice(1)
+
     return <div className='app-header p-5 text-center text-md-start'>
-        <h1 className='py-4'>Piątek, 16 czerwca</h1>
+        <h1 className='py-4'>{formatedDate}</h1>
     </div>
 }
 
