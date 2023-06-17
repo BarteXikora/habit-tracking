3d72cdd19427360fa6c56bb9da221b966fd0a351
BarteXikora <sbartek50@gmail.com>
2023-06-17 23:02:49 +0200

    Added styles to habits left mark.

--- client/src/components/sections/HabitsCardsSection.tsx
+++ client/src/components/sections/HabitsCardsSection.tsx
@@ -1,14 +1,14 @@
 import { Row, Col } from 'react-bootstrap'
 
 const HabitsCardsSection = () => {
-    const __left: number = 0
+    const __left: number = 2
 
     return <Row className='px-4 py-5'>
-        <Col xs={12}>
-            <h2>Śledzone nawyki:</h2>
+        <Col xs={12} className='d-flex align-items-center mb-3'>
+            <h2 className='m-0'>Śledzone nawyki:</h2>
 
-            <div className={`title-left-habits ${__left > 0 && 'title-left-habits-warn'}`}>
-                {__left > 0 ? __left + ' do uzupełnienia' : 'Gotowe!'}
+            <div className={`title-left-habits ${__left > 0 && 'title-left-habits-warn'} py-1 px-3 ms-3`}>
+                {__left > 0 ? __left + ' do uzupełnienia!' : 'Gotowe!'}
             </div>
         </Col>
     </Row>
