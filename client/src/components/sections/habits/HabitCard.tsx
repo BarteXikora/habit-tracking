b441df47cf840b9880b6eb92508476f2a6f4051d
BarteXikora <sbartek50@gmail.com>
2023-06-27 11:29:44 +0200

    Devided habit card component into 2 different components.

--- client/src/components/sections/habits/HabitCard.tsx
+++ client/src/components/sections/habits/HabitCard.tsx
@@ -1,26 +1,15 @@
-import { useState } from 'react'
-import { Col, Button, FormControl } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
+import { Habits } from '../../../features/habits'
+import OptionSelected from './OptionSelected'
+import SelectOption from './SelectOption'
 
 import iconNoIcon from '../../../img/icon-no-icon.svg'
-import iconDone from '../../../img/icon-done.svg'
-import iconNotDone from '../../../img/icon-not-done.svg'
-import iconNotApplicable from '../../../img/icon-not-applicable.svg'
-import iconDoneColored from '../../../img/icon-done-colored.svg'
-import iconNotDoneColored from '../../../img/icon-not-done-colored.svg'
-import iconNotApplicableColored from '../../../img/icon-not-applicable-colored.svg'
-import iconNoteAdded from '../../../img/icon-note-added.svg'
 
-import { useAppDispatch } from '../../../functions/reduxStore'
-import { selectOption, Habits, addNote } from '../../../features/habits'
-
-interface IHabitCard {
+export interface IHabitCard {
     habit: Habits
 }
 
 const HabitCard = ({ habit }: IHabitCard) => {
-    const [note, setNote] = useState<string>('')
-    const dispatch = useAppDispatch()
-
     return <Col md={4} className='d-grid px-2 pb-4'>
         <div className={`
             habit-card-${habit.selectedOption ?
@@ -48,97 +37,11 @@ const HabitCard = ({ habit }: IHabitCard) => {
             {
                 habit.selectedOption ?
 
-                    <>
-                        <span className="d-flex align-items-center fw-bold">
-                            {
-                                habit.selectedOption === 'done' ?
-
-                                    <div className='font-good'>
-                                        <img src={iconDoneColored} alt="Wykonano" width={25} className='me-1' />
-                                        Zadanie wykonane!
-                                    </div>
-
-                                    :
-
-                                    habit.selectedOption === 'not-done' ?
-
-                                        <div className='font-wrong'>
-                                            <img src={iconNotDoneColored} alt="Nie wykonano" width={25} className='me-1' />
-                                            Zadanie niewykonane!
-                                        </div>
-
-                                        :
-
-                                        <div className='font-gray'>
-                                            <img src={iconNotApplicableColored} alt="Nie dotyczy" width={25} className='me-1' />
-                                            Dziś nie dotyczy!
-                                        </div>
-                            }
-                        </span>
-
-                        {
-                            habit.note ?
-                                <div className='d-flex align-items-center mt-2 font-gray'>
-                                    <img src={iconNoteAdded} alt="Dodano notatkę!" className='me-1' />
-                                    <span>
-                                        {
-                                            habit.note.length < 80 ?
-                                                habit.note
-                                                :
-                                                habit.note.substring(0, 80) + '...'
-                                        }
-                                    </span>
-                                </div>
-
-                                :
-
-                                <>
-                                    <FormControl
-                                        placeholder='Możesz dodać notatkę...'
-                                        className='mt-3'
-                                        value={note}
-                                        onChange={e => setNote(e.target.value)}
-                                    />
-
-                                    {
-                                        note.length > 0 && <Button
-                                            className="btn btn-prim mt-2"
-                                            onClick={() => dispatch(addNote({ id: habit.id, note }))}
-                                        >
-                                            Dodaj notatkę!
-                                        </Button>
-                                    }
-                                </>
-                        }
-                    </>
+                    <OptionSelected habit={habit} />
 
                     :
 
-                    <>
-                        <span className="fw-bold d-block mb-2">Dostępne oznaczenia:</span>
-
-                        {
-                            habit.options.length > 0 ?
-
-                                habit.options.map(option => {
-                                    return <Button
-                                        className={`btn btn-option btn-option-${option} p-2 d-inline-block me-2 mb-1`}
-                                    >
-                                        <img
-                                            src={
-                                                option === 'done' ? iconDone : option === 'not-done' ? iconNotDone : iconNotApplicable
-                                            }
-                                            alt=""
-                                            onClick={() => dispatch(selectOption({ id: habit.id, option }))}
-                                        />
-                                    </Button>
-                                })
-
-                                :
-
-                                <>Brak dostępnych opcji</>
-                        }
-                    </>
+                    <SelectOption habit={habit} />
             }
         </div>
     </Col>
---
+++ client/src/components/sections/habits/SelectOption.tsx
@@ -0,0 +1,40 @@
+import { Button } from 'react-bootstrap'
+import { IHabitCard } from './HabitCard'
+import { useAppDispatch } from '../../../functions/reduxStore'
+import { selectOption } from '../../../features/habits'
+
+import iconDone from '../../../img/icon-done.svg'
+import iconNotDone from '../../../img/icon-not-done.svg'
+import iconNotApplicable from '../../../img/icon-not-applicable.svg'
+
+const SelectOption = ({ habit }: IHabitCard) => {
+    const dispatch = useAppDispatch()
+
+    return <>
+        <span className="fw-bold d-block mb-2">Dostępne oznaczenia:</span>
+
+        {
+            habit.options.length > 0 ?
+
+                habit.options.map(option => {
+                    return <Button
+                        className={`btn btn-option btn-option-${option} p-2 d-inline-block me-2 mb-1`}
+                    >
+                        <img
+                            src={
+                                option === 'done' ? iconDone : option === 'not-done' ? iconNotDone : iconNotApplicable
+                            }
+                            alt=""
+                            onClick={() => dispatch(selectOption({ id: habit.id, option }))}
+                        />
+                    </Button>
+                })
+
+                :
+
+                <>Brak dostępnych opcji</>
+        }
+    </>
+}
+
+export default SelectOption
