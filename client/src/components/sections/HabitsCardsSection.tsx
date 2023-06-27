419f3f7e42796289c7f6b067e8b1fb4633d8604e
BarteXikora <sbartek50@gmail.com>
2023-06-27 11:17:04 +0200

    Created counting left habits function.

--- client/src/components/sections/HabitsCardsSection.tsx
+++ client/src/components/sections/HabitsCardsSection.tsx
@@ -1,17 +1,32 @@
+import { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import HabitCard from './habits/HabitCard'
 import { useAppSelector } from '../../functions/reduxStore'
 
 const HabitsCardsSection = () => {
-    const __left: number = 2
     const habitsList = useAppSelector(state => state.habits)
 
+    const countLeft = (): number => {
+        let cnt = 0
+
+        habitsList.forEach(habit => { if (!habit.selectedOption) cnt++ })
+
+        return cnt
+    }
+
+    const [left, setLeft] = useState<number>(countLeft())
+
+    useEffect(() => {
+        setLeft(countLeft())
+
+    }, [habitsList])
+
     return <Row className='px-4 py-5'>
-        <Col xs={12} className='d-flex align-items-center mb-3'>
+        <Col xs={12} className='d-flex align-items-center mb-4'>
             <h2 className='m-0'>Śledzone nawyki:</h2>
 
-            <div className={`title-left-habits ${__left > 0 && 'title-left-habits-warn'} py-1 px-3 ms-3`}>
-                {__left > 0 ? __left + ' do uzupełnienia!' : 'Gotowe!'}
+            <div className={`title-left-habits ${left > 0 && 'title-left-habits-warn'} py-1 px-3 ms-3`}>
+                {left > 0 ? left + ' do uzupełnienia!' : 'Gotowe!'}
             </div>
         </Col>
 
