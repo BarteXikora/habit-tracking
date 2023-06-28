e3d6baf3b1f238ff5593f38daf14ebfe71a9cffc
BarteXikora <sbartek50@gmail.com>
2023-06-28 17:29:22 +0200

    Added onclick to step by step button in header.

--- client/src/components/sections/Header.tsx
+++ client/src/components/sections/Header.tsx
@@ -1,7 +1,8 @@
 import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 import iconStepByStep from '../../img/icon-step-by-step.svg'
-import iconOverdue from '../../img/icon-time.svg'
+// import iconOverdue from '../../img/icon-time.svg'
 import iconDone from '../../img/icon-done-white.svg'
 
 import moment from 'moment'
@@ -11,22 +12,27 @@ const Header = () => {
     const date: string = moment().format('dddd, D MMMM')
     const formatedDate = date.charAt(0).toUpperCase() + date.slice(1)
 
+    const navigate = useNavigate()
+
     return <div className='app-header p-5 text-center text-md-start'>
         <div className="py-3">
             <h1 className='py-2 m-0'>{formatedDate}</h1>
 
             <div className="d-flex mt-3">
-                <Button className='btn btn-white btn-icon-text me-3 mb-1'>
+                <Button
+                    className='btn btn-white btn-icon-text me-3 mb-1'
+                    onClick={() => navigate('/step-by-step')}
+                >
                     <img src={iconStepByStep} alt="Przejdź do uzupełniania dnia" />
 
                     Uzupełnij ten dzień!
                 </Button>
 
-                <Button className='btn btn-warn btn-icon-text me-3 mb-1'>
+                {/* <Button className='btn btn-warn btn-icon-text me-3 mb-1'>
                     <img src={iconOverdue} alt="Uzupełnij zaległe dni" />
 
                     Uzupełnij zaległe!
-                </Button>
+                </Button> */}
             </div>
 
             <div className="__d-flex d-none align-items-center">
